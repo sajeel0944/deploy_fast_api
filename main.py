0560721709522e2247b01c9086f928a6b1f54291
sajeek0944 <sajeel6y4z@gmail.com>
2025-06-20 21:35:13 +0500

    fast api

--- main.py
+++ main.py
@@ -2,7 +2,35 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/", status_code=200)
+@app.get("/")
 def hello_word():
     """Basic root endpoint"""
     return {"message": "well to sajeel"}
+
+
+
+@app.get("/new")
+def hello_word():
+    """Basic root endpoint"""
+    return {"message": "new user in login"}
+
+
+@app.post("/post")
+def hello_word():
+    """Basic root endpoint"""
+    return {"message": "this is post request"}
+
+
+
+@app.put("/put")
+def hello_word():
+    """Basic root endpoint"""
+    return {"message": "this is put request"}
+
+
+
+@app.delete("/delete")
+def hello_word():
+    """Basic root endpoint"""
+    return {"message": "this is delete request"}
+
